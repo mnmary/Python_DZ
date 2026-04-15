3e2541c2b20e7474f8f2a137f8f4e5b75554b2b9
MN <mn@ya.ru>
2026-04-15 08:06:57 +0300

    refactoring

--- ADV_HW06/main.py
+++ ADV_HW06/main.py
@@ -1,6 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_link(keyword, snippet):
+    if snippet.text.lower().find(keyword) >= 0:
+        data = snippet.find('time').get('title')
+        header = snippet.find('h2')
+        if not header is None:
+            header_span = header.find('span').text
+            header_link = header.find('a').get('href')
+        return(f'{data}-{header_span}-{"https://habr.com" + header_link}')
+    else:
+        return ''
+
 
 def main():
 
@@ -14,14 +25,12 @@ def main():
         all_snippets = soup.find_all(class_='article-snippet')
         for snippet in all_snippets:
             for keyword in KEYWORDS:
-                if snippet.text.lower().find(keyword) >= 0:
-                    data = snippet.find('time').get('title')
-                    header = snippet.find('h2')
-                    if not header is None:
-                        header_span = header.find('span').text
-                        header_link = header.find('a').get('href')
-                    print(f'{data}-{header_span}-{"https://habr.com" + header_link}')
+                result = get_link(keyword, snippet)
+                if result:
+                        print(result)
     except Exception as e:
         print(e)
+
+
 if __name__ == '__main__':
     main()
