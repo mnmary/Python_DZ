6853d3b5a53af69dcb36f16002d984a570db374f
MN <mn@ya.ru>
2026-04-16 10:55:41 +0300

    bug fix

--- ADV_HW02/iterator.py
+++ ADV_HW02/iterator.py
@@ -1,31 +1,53 @@
 import types
 
 class FlatIterator:
+    """
+    Ваше решение близко к правильному, но здесь есть критичная проблема с ленивостью FlatIterator:
+    при инициализации Вы сразу строите полный плоский список.
+    Для этой задачи итератор должен обходить вложенность по мере запроса следующего элемента,
+    а не заранее хранить весь результат.
+
+    Что стоит проверить:
+
+    Что происходит внутри __init__ — создаётся ли там уже готовый список всех элементов?
+    Как ведёт себя __next__, если вход очень большой или бесконечно вложенный?
+    Можно ли получить первый элемент, не дожидаясь полной обработки всей структуры?
+
+    Подсказка по направлению исправления:
+
+    У итератора лучше хранить текущее состояние обхода: позицию во внешнем списке и внутри вложенных списков
+    __iter__() должен возвращать self
+    __next__() — последовательно находить следующий атомарный элемент, не вычисляя всё заранее.
+    Для самопроверки:
+
+    Сравните поведение list(FlatIterator(...)) и обхода через next()
+    Убедитесь, что в памяти не создаётся готовая «плоская копия» входного списка
+    Отдельно проверьте случаи с пустыми вложенными списками.
+    """
 
     def __init__(self, list_of_list):
-        def create_flat_list(list_of):
-            tmp_list = []
-            for item in list_of:
-                if isinstance(item, list):
-                    # is list
-                    tmp_list.extend(create_flat_list(item))
-                else:
-                    tmp_list.append(item)
-            return tmp_list
-
-        self.list_of_list = create_flat_list(list_of_list)
+        self.list_of_list = list_of_list    # исходные данные - неплоский список
+        self._stack = []                    # стек итераторов для отслеживания положения в списках
 
     def __iter__(self):
-        self.cursor = -1
-        self.end = len(self.list_of_list)
+        self._stack = [iter(self.list_of_list)]  # кладем в стек итератор исходного списка
         return self
 
     def __next__(self):
-        self.cursor += 1
-        if self.cursor == self.end:
-            raise StopIteration
-        else:
-            return self.list_of_list[self.cursor]
+        while self._stack:  # проходим стек пошагово пока не опустеет
+            try:
+                item = next(self._stack[-1])  # берем следующий элемент из списка по последнему итератору из стека
+            except StopIteration:
+                self._stack.pop()   # если итератор закончился - чистим стек от него
+                continue
+
+            if isinstance(item, list):
+                self._stack.append(iter(item))  # если это список - кладем его итератор в стек
+                continue
+
+            return item # возвращаем итератор и уходим до следующего шага
+
+        raise StopIteration
 
 
 def flat_generator(list_of_lists):
@@ -48,6 +70,7 @@ def test_1():
         [1, 2, None, [[[[['!']]]]], []]
     ]
     print(list(FlatIterator(list_of_lists_1)))
+    print(list(FlatIterator(list_of_lists_2)))
 
     for flat_iterator_item, check_item in zip(
             FlatIterator(list_of_lists_1),
@@ -71,6 +94,7 @@ def test_2():
         [1, 2, None, [[[[['!']]]]], []]
     ]
     print(list(flat_generator(list_of_lists_1)))
+    print(list(flat_generator(list_of_lists_2)))
     for flat_iterator_item, check_item in zip(
             flat_generator(list_of_lists_1),
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
